01795b6be87711289fa78bc02444cb62d665e105
unknown <aobordug@gmail.com>
2020-11-06 20:59:17 +0500

    модификация моя

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 class Sneakers:
-    def __init__(self, name, count):
+    def __init__(self, name, count, manufacturer, price, size):
         self.name = name
         self.count = count
+        self.manufacturer = manufacturer
+        self.price = price
+        self.size = size
 
 
 class Warehouse:
@@ -9,9 +12,10 @@ class Warehouse:
         self._id = 0
         self._sneakers = dict()
 
-    def add(self, sneakers):
-        self._id = self._id + 1
-        self._sneakers[self._id] = sneakers
+    def add(self, *sneakers):
+        for sneaker in sneakers:
+            self._id += 1
+            self._sneakers[self._id] = sneaker
 
     def remove(self, sneakers):
         if sneakers is not None:
@@ -22,21 +26,37 @@ class Warehouse:
     def display(self):
         print("Товары на складе:")
         for key, value in self._sneakers.items():
-            print(f'id - {key};name - {value.name};count - {value.count}')
+            print(f'id - {key};name - {value.name};count - {value.count}; '
+                  f'manufacturer - {value.manufacturer}; price - {value.price}; size - {value.size};')
+
+
+def CreateSneakers():
+    print('введите название кросовок')
+    name = input()
+    print('введите количество')
+    count = int(input())
+    return Sneakers(name, count)
 
 
 warehouse = Warehouse()
 while True:
-    print(f'1-добавить товар\n2-отобразить все товары\n3-выход')
+    print(f'1-добавить товар\n2-отобразить все товары\n3-удалить\n4-выход')
     choice = int(input())
     if choice == 1:
-        print('введите название кросовок')
-        name = input()
-        print('введите количество')
-        count = int(input())
-        sneakers = Sneakers(name, count)
-        warehouse.add(sneakers)
-    if choice == 2:
+        sneakers = list()
+        while True:
+            sneaker = CreateSneakers()
+            sneakers.append(sneaker)
+            print('\t1-добавить ещё товар\n\t2-закончить ввод товаров')
+            ans = int(input())
+            if ans == 2:
+                break
+        warehouse.add(sneaker)
+    elif choice == 2:
         warehouse.display()
-    if choice == 3:
+    elif choice == 3:
+        warehouse.display()
+        sneakers = CreateSneakers()
+        warehouse.remove(sneakers)
+    if choice == 4:
         break
